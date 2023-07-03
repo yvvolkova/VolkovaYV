dc3495bc9dc6bfaf9b561dde95a4143f4397c1cc
volkova.yu <volkova.yu@cdek.ru>
2023-07-03 20:25:55 +0500

    отформатировала код

--- Main.java
+++ Main.java
@@ -2,9 +2,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args)
-
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String userInput = "";
         while (!"q".equals(userInput)) {
@@ -18,55 +16,59 @@ public class Main {
                 userInput = scanner.nextLine();
                 double result;
 
-            switch (userInput){
-                case "+":
-                    result = plus(firstNumber, secondNumber);
-                    break;
-                case "-":
-                    result = minus(firstNumber,secondNumber);
-                    break;
-                case "*":
-                    result = ymn(firstNumber, secondNumber);
-                    break;
-                case "!":
-                    result = fact(firstNumber);
-                    break;
-                default:
-                    result = 0;
-                    break;
-                case  "/":
-                    if (secondNumber == 0) {
-                        continue;
-                    }
-                    result = del(firstNumber, secondNumber);
-                    break;
-            }
-            System.out.println("Результат вычисления = " + result);
+                switch (userInput) {
+                    case "+":
+                        result = plus(firstNumber, secondNumber);
+                        break;
+                    case "-":
+                        result = minus(firstNumber, secondNumber);
+                        break;
+                    case "*":
+                        result = ymn(firstNumber, secondNumber);
+                        break;
+                    case "!":
+                        result = fact(firstNumber);
+                        break;
+                    default:
+                        result = 0;
+                        break;
+                    case "/":
+                        if (secondNumber == 0) {
+                            continue;
+                        }
+                        result = del(firstNumber, secondNumber);
+                        break;
+                }
+                System.out.println("Результат вычисления = " + result);
             } catch (InputMismatchException ex) {
                 System.out.println("Введите только цифры, без букв и символов");
                 scanner.nextLine();
                 continue;
+            }
         }
     }
-}
 
-        private static int plus (int numberOne, int numberTwo) {
-            return numberOne + numberTwo;
-        }
-        private static int minus (int numberOne, int numberTwo) {
+    private static int plus(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
+    }
+
+    private static int minus(int numberOne, int numberTwo) {
         return numberOne - numberTwo;
-        }
-        private static int ymn (int numberOne, int numberTwo) {
+    }
+
+    private static int ymn(int numberOne, int numberTwo) {
         return numberOne * numberTwo;
-        }
-        private static int del(int numberOne, int numberTwo) {
+    }
+
+    private static int del(int numberOne, int numberTwo) {
         return numberOne / numberTwo;
-         }
-         private static int fact(int numberOne) {
-             int res = 1;
-             for (int i = 1; i < numberOne; i++) {
-                res *= i;
+    }
+
+    private static int fact(int numberOne) {
+        int res = 1;
+        for (int i = 1; i < numberOne; i++) {
+            res *= i;
         }
-            return res;
+        return res;
     }
 }
